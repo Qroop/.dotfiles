a00f2e20d7e1417a25d6319beebd1fadf5ce1ddb
jeswa278 <jeswa278@student.liu.se>
2025-04-24 10:40:27 +0200

    Dont know

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,3 +3,10 @@
 -- Add any additional keymaps here
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
+
+-- Center screen after search next/prev
+vim.keymap.set("n", "n", "nzzzv", { noremap = true })
+vim.keymap.set("n", "N", "Nzzzv", { noremap = true })
+-- Center screen after first search result
+-- vim.keymap.set("n", "/", "/zzzv", { noremap = true })
+-- vim.keymap.set("n", "?", "?zzzv", { noremap = true })
